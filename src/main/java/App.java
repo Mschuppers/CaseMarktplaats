c99a22f21acb16a8bb12eac9d2314a95466ae88d
Mschuppers <m.schuppers@belastingdienst.nl>
2021-11-01 14:23:27 +0100

    comit v0.1.0002a [CR-001] PRODUCTMENU HIERARCHY ADDED

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,77 @@
+import dao.ProductDao;
+import products.Product;
+import lombok.var;
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
+import org.slf4j.Logger;
+
+import javax.inject.Inject;
+import java.util.List;
+import java.util.Scanner;
+
+
+
 public class App {
+    @Inject
+    private Logger log;
+
+    @Inject
+    private ProductDao dao;
+
+    @Inject
+    private Scanner sc;
+
 
     public static void main(String[] args) {
-        App.start();
+        Weld weld = new Weld();
+        WeldContainer weldContainer = weld.initialize();
+        App app = weldContainer.select(App.class).get();
+        while (true) {
+            app.start();
+        }
+    }
+
+
+    void start() {
+        log.info("Welcome to the Product menu");
+        System.out.println("Choose option");
+        System.out.println("1) Choose find 1 product by ID");
+        System.out.println("2) Choose find all available products");
+        System.out.println("3) Add a new product");
+        System.out.println("4) Update an excisting product");
+        System.out.println("5) Remove a product");
+
+
+        var input = sc.nextInt();
+        switch (input) {
+            case 1:
+                System.out.println("Find which id?");
+                int id = sc.nextInt();
+                dao.findProduct(id);
+                break;
+            case 2:
+                dao.findAllProducts();
+                List<Product> all = dao.findAllProducts();
+                all.forEach(System.out::println);
+                break;
+            case 3:
+                dao.insertProduct();
+                break;
+            case 4:
+                int pId = sc.nextInt();
+                System.out.println("Update which id?");
+                dao.updateProduct(dao.findProduct(pId));
+                break;
+            case 5:
+                System.out.println("Delete which id?");
+                id = sc.nextInt();
+                Product x = dao.findProduct(id);
+                dao.deleteProduct(x);
+                break;
+            default:
+                System.out.println("default");
+        }
     }
 }
+
+
