089ee2b643c8860dd204c1e9068d2c8add3baa40
Mschuppers <m.schuppers@belastingdienst.nl>
2021-11-02 13:38:08 +0100

    comit v0.1.0009a [CR-001] Menu is exitable now

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,10 +2,8 @@ import dao.ProductDao;
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
 import org.slf4j.Logger;
-import products.Product;
 
 import javax.inject.Inject;
-import java.util.List;
 import java.util.Scanner;
 
 
@@ -24,52 +22,58 @@ public class App {
         Weld weld = new Weld();
         WeldContainer weldContainer = weld.initialize();
         App app = weldContainer.select(App.class).get();
-        while (true) {
-            app.start();
-        }
+
+        app.start();
     }
 
 
-    void start() {
-        log.info("Welcome to the Product menu");
-        System.out.println("Choose option");
-        System.out.println("1) Choose find 1 product by ID");
-        System.out.println("2) Choose find all available products");
-        System.out.println("3) Add a new product");
-        System.out.println("4) Update an excisting product");
-        System.out.println("5) Remove a product");
+    boolean start() {
+
+        while (true) {
+            log.info("Welcome to the Product menu");
+            System.out.println("Choose option");
+            System.out.println("1) Choose find 1 product by ID");
+            System.out.println("2) Choose find all available products");
+            System.out.println("3) Add a new product");
+            System.out.println("4) Update an excisting product");
+            System.out.println("5) Remove a product");
 
 
-        int input = Integer.parseInt(sc.nextLine());
-        switch (input) {
-            case 1:
-                System.out.println("Find which id?");
-
-                dao.findProduct(Integer.parseInt(sc.nextLine()));
-                break;
-            case 2:
+            int input = Integer.parseInt(sc.nextLine());
+            switch (input) {
+                case 1:
+//                System.out.println("Find which id?");
+//
+//                dao.findProduct(Integer.parseInt(sc.nextLine()));
+//                break;
+                case 2:
 //                dao.findAllProducts();
 //                List<Product> all = dao.findAllProducts();
 //                all.forEach(System.out::println);
-                break;
-            case 3:
-//                dao.insertProduct();
-//                break;
-            case 4:
+                    break;
+                case 3:
+                    dao.insertProduct();
+                    break;
+
+                case 4:
 //                int pId = sc.nextInt();
 //                System.out.println("Update which id?");
 //                dao.updateProduct(dao.findProduct(pId));
 //                break;
-            case 5:
+                case 5:
 //                System.out.println("Delete which id?");
 //                id = sc.nextInt();
 //                Product x = dao.findProduct(id);
 //                dao.deleteProduct(x);
 //                break;
-            default:
-                System.out.println("default");
+                case 0:
+                    System.out.println("Exiting application");
+                    return false;
+                default:
+                    System.out.println("default");
+            }
+
         }
     }
 }
-
 
